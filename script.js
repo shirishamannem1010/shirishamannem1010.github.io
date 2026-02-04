f1d1daf9e8e68c0e2b327032d8ade62eb49221d7
shirisha mannem <shirisha7143@gmail.com>
2026-02-04 16:21:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
